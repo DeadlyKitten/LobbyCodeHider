50bf54a59fb352f0885bfb99068a223034490357
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-10-19 01:53:17 -0700

    Now hides lobby code on lobby search screen.

---
+++ HideLobbyCode/Patches/OnlineSearchSpecificLobbyContent_MenuOpen.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Nick;
+
+namespace HideLobbyCode.Patches
+{
+    [HarmonyPatch(typeof(OnlineSearchSpecificLobbyContent), "MenuOpen")]
+    class OnlineSearchSpecificLobbyContent_MenuOpen
+    {
+        static void Postfix(ref MenuTextInput ____input) => ____input.Input.contentType = TMPro.TMP_InputField.ContentType.Password;
+    }
+}
---
+++ HideLobbyCode/Patches/OnlineSearchSpecificLobbyContent_SearchLobby.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nick;
+using HarmonyLib;
+using TMPro;
+using System.Reflection.Emit;
+using System.Linq;
+
+namespace HideLobbyCode.Patches
+{
+    [HarmonyPatch(typeof(OnlineSearchSpecificLobbyContent), "SearchLobby")]
+    class OnlineSearchSpecificLobbyContent_SearchLobby
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+        {
+            var instructions = codes.ToList();
+
+            var currentInstruction = instructions.FindIndex(x => x.opcode == OpCodes.Callvirt);
+
+            var getText = typeof(TMP_InputField).GetProperty("text").GetGetMethod();
+
+            instructions[++currentInstruction].operand = getText;
+            instructions.RemoveAt(++currentInstruction);
+
+            return instructions;
+        }
+    }
+}
